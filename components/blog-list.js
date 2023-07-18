2beb8fdfbcb4f814f5e11113b9b2614ccc66a111
James Lovallo <lovallo.james@gmail.com>
2023-07-18 01:02:15 -0400

    .

--- components/blog-list.js
+++ components/blog-list.js
@@ -3,6 +3,10 @@ import ardi, { css, html } from '//unpkg.com/ardi'
 
 ardi({
 	tag: 'blog-list',
+	ready() {
+		const app = this.context('app')
+		app.handleLinks(this.root)
+	},
 	template() {
 		return html`
 			<ul>
@@ -25,13 +29,23 @@ ardi({
 	},
 	styles: css`
 		ul {
+			display: grid;
+			gap: 1.5rem;
 			list-style: none;
 			padding: 0;
+		}
+		li {
+			line-height: 1.5;
+		}
+		h3,
+		p {
+			margin: 0;
 		}
 		[part='meta'] {
 			align-items: baseline;
 			display: flex;
 			justify-content: space-between;
+			margin-top: 0.5rem;
 		}
 	`,
 })
